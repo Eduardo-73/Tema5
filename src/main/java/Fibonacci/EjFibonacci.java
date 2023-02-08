65b5c436c1f3867aaa8a85bf44261d6a4716de81
Eduardo <eduardocorreaaranda@gmail.com>
2023-02-08 14:39:27 +0100

    ver en casa

---
+++ src/main/java/Fibonacci/EjFibonacci.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Fibonacci;
+
+/**
+ *
+ * @author eduardo
+ */
+public class EjFibonacci {
+
+    public static void main(String[] args) {
+        final int NUM0 = 0;
+        final int NUM1 = 1;
+        int resultado;
+        resultado = NUM0 + NUM1;
+        for (int i = 0; i < 10; i++) {
+            if(NUM0 == 0 && NUM1 == 1){
+               int resul2 = resultado + NUM1; 
+            }
+        }
+    }
+}

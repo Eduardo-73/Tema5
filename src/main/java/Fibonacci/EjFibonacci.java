9ac8d300c19b6929be16bc04dcae689bad26d7c8
Eduardo <eduardocorreaaranda@gmail.com>
2023-02-09 12:43:25 +0100

    seguir en casa

--- src/main/java/Fibonacci/EjFibonacci.java
+++ src/main/java/Fibonacci/EjFibonacci.java
@@ -9,16 +9,34 @@ package Fibonacci;
  * @author eduardo
  */
 public class EjFibonacci {
-
+    
     public static void main(String[] args) {
-        final int NUM0 = 0;
-        final int NUM1 = 1;
-        int resultado;
-        resultado = NUM0 + NUM1;
-        for (int i = 0; i < 10; i++) {
-            if(NUM0 == 0 && NUM1 == 1){
-               int resul2 = resultado + NUM1; 
-            }
+        int n = fiboIterativo(5);
+        System.out.println(n);
+        System.out.println("");
+        System.out.println(fiboRecursivo(5));
+    }
+    
+    public static int fiboIterativo(int n) {
+        int numero = Math.abs(n);
+        
+        int resultado = 0;
+        int n1 = 0;
+        int n2 = 1;
+        
+        for (int i = 0; i < numero; i++) {
+            resultado = n1 + n2;
+            n2 = n1;
+            n1 = resultado;
+        }
+        return resultado;
+    }
+    
+    public static int fiboRecursivo(int n) {//Suponemos que n es positivo o 0
+        if (n < 2) {
+            return n;
+        } else {
+            return fiboRecursivo(n - 1) + fiboRecursivo(n - 2);
         }
     }
 }
